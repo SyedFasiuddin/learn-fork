ae3895080da5a3adc4d78e80c11aef34bb014867
Syed Fasiuddin <66054777+SyedFasiuddin@users.noreply.github.com>
2023-03-25 11:20:43 +0530

    forking the process just like that

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    use fork::{fork, Fork};
+
+    match fork() {
+        Ok(Fork::Parent(child_pid)) => {
+            println!("Child pid: {child_pid}");
+            println!("Parent continue executing");
+        }
+        Ok(Fork::Child) => {
+            println!("In child Process");
+        }
+        Err(e) => eprintln!("Fork failed due to {e}"),
+    }
 }
